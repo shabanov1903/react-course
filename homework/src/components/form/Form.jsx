808489ffe94cca9ee85264c337e48e37db7cd2e7
Danil Shabanov <dvshabanov@baltbet.ru>
2022-08-18 16:51:56 +0300

    Virtual DOM. Material UI. PropTypes

--- homework/src/components/form/Form.jsx
+++ homework/src/components/form/Form.jsx
@@ -1,5 +1,10 @@
 import './Form.scss';
-import { useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import { useState, useRef } from 'react';
+import { useTheme } from '@mui/material/styles';
 
 export default function Form(props) {
 
@@ -8,8 +13,12 @@ export default function Form(props) {
   const [author, setAuthor] = useState('');
   const [text, setText] = useState('');
 
+  const textRef = useRef(null);
+  const theme = useTheme();
+
   const click = () => {
     fill(author, text, "user");
+    focus();
   }
 
   function fill(author, text, sender) {
@@ -22,14 +31,34 @@ export default function Form(props) {
     }]);
     setText('');
   }
+
+  function focus() {
+    textRef.current.focus()
+  }
   
   return(
     <div>
-      <label>Автор</label> <br/>
-      <input type="text" value={author} onChange={(event) => setAuthor(event.target.value)}></input> <br/>
-      <label>Текст</label> <br/>
-      <input type="text" value={text} onChange={(event) => setText(event.target.value)}></input> <br/>
-      <button onClick={click}>Отправить</button> <br/>
+      <Box
+        component="form"
+        sx={{ bgcolor: theme.background, borderRadius: 2 }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          label="Автор"
+          variant="outlined"
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+          autoFocus={true}
+          sx={{marginTop: "10px"}}/>
+        <TextField
+          label="Текст"
+          variant="outlined"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          inputRef={textRef}/>
+        <Button variant="outlined" endIcon={<SendIcon/>} onClick={click} className="send-button">Отправить</Button>
+      </Box>
     </div>
   );
 }
